bfe69d541c66e75c512a26ce10e423d5957b3139
Harpica <harpy.ka@gmail.com>
2023-03-16 19:32:35 +0300

    feat: allow all origins

--- server/server.ts
+++ server/server.ts
@@ -12,7 +12,7 @@ const app = express();
 
 const PORT = process.env.APP_PORT || 5000;
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: '*',
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
